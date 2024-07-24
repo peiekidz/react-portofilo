9bd08324f322fdd1f5c30ffc450f55fb06a34855
Fery febriawan <peiemokidz@gmail.com>
2024-07-24 17:08:33 +0700

    change logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/logowhite.png"
+import logo from "../assets/logo.png"
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -6,7 +6,7 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
     return <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center" >
-            <img className="mx-3 w-24" src={logo} alt=""/>
+            <img className="mx-3 w-14 filter invert" src={logo} alt=""/>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl cursor-pointer">
             <a href="https://www.linkedin.com/in/fery-febriawan-858943166/"><FaLinkedin/></a>  
